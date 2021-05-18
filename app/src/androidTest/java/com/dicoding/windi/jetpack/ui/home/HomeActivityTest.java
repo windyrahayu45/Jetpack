a660f98ce0f4ff4123118f263eebe7a5bd6b21f4
kominfo <windyrahayuuu@gmail.com>
2021-05-18 11:24:27 +0700

    final

--- app/src/androidTest/java/com/dicoding/windi/jetpack/ui/home/HomeActivityTest.java
+++ app/src/androidTest/java/com/dicoding/windi/jetpack/ui/home/HomeActivityTest.java
@@ -1,5 +1,6 @@
 package com.dicoding.windi.jetpack.ui.home;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -10,7 +11,10 @@ import com.dicoding.windi.jetpack.utils.DataDummy;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -27,27 +31,62 @@ public class HomeActivityTest  {
     public ActivityScenarioRule activityRule = new ActivityScenarioRule<>(HomeActivity.class);
 
     @Test
-    public void loadCourses() {
+    public void loadMovie() {
         onView(withId(R.id.rv_movies)).check(matches(isDisplayed()));
         onView(withId(R.id.rv_movies)).perform(RecyclerViewActions.scrollToPosition(dataEntityArrayList.size()));
     }
 
     @Test
-    public void loadDetailCourse() {
+    public void loadDetailMovie() {
         onView(withId(R.id.rv_movies)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(withId(R.id.text_title)).check(matches(isDisplayed()));
         onView(withId(R.id.text_title)).check(matches(withText(dataEntityArrayList.get(0).getTitle())));
         onView(withId(R.id.text_date)).check(matches(isDisplayed()));
+        onView(withId(R.id.text_date)).check(matches(withText(parseDateToddMMyyyy(dataEntityArrayList.get(0).getReleaseDate()))));
+        onView(withId(R.id.text_description)).check(matches(isDisplayed()));
+        onView(withId(R.id.text_description)).check(matches(withText(dataEntityArrayList.get(0).getDescription())));
+        onView(withId(R.id.text_vote)).check(matches(isDisplayed()));
+        onView(withId(R.id.text_vote)).check(matches(withText(dataEntityArrayList.get(0).getVote())));
+        onView(withId(R.id.image_poster)).check(matches(isDisplayed()));
 
     }
 
-
-
     @Test
-    public void loadBookmarks() {
+    public void loadShow() {
         onView(withText("TV Show")).perform(click());
         onView(withId(R.id.rv_show)).check(matches(isDisplayed()));
         onView(withId(R.id.rv_show)).perform(RecyclerViewActions.scrollToPosition(dataShow.size()));
+    }
+
+    @Test
+    public void loadDetailShow() {
+        onView(withText("TV Show")).perform(click());
+        onView(withId(R.id.rv_show)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.text_title)).check(matches(isDisplayed()));
+        onView(withId(R.id.text_title)).check(matches(withText(dataShow.get(0).getTitle())));
+        onView(withId(R.id.text_date)).check(matches(isDisplayed()));
+        onView(withId(R.id.text_date)).check(matches(withText(parseDateToddMMyyyy(dataShow.get(0).getReleaseDate()))));
+        onView(withId(R.id.text_description)).check(matches(isDisplayed()));
+        onView(withId(R.id.text_description)).check(matches(withText(dataShow.get(0).getDescription())));
+        onView(withId(R.id.text_vote)).check(matches(isDisplayed()));
+        onView(withId(R.id.text_vote)).check(matches(withText(dataShow.get(0).getVote())));
+        onView(withId(R.id.image_poster)).check(matches(isDisplayed()));
+
+    }
+
+    public String parseDateToddMMyyyy(String strCurrentDate) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date newDate = null;
+        try {
+            newDate = format.parse(strCurrentDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        format = new SimpleDateFormat("MMM dd, yyyy");
+        String date = format.format(newDate);
+
+        return date;
     }
 
 }
