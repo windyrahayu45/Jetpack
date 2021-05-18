13f05db5acf72cc5d2801731ee325e8e1eeaa80e
kominfo <windyrahayuuu@gmail.com>
2021-05-18 10:28:02 +0700

    final

--- app/src/main/java/com/dicoding/windi/jetpack/utils/DataDummy.java
+++ app/src/main/java/com/dicoding/windi/jetpack/utils/DataDummy.java
@@ -109,7 +109,7 @@ public class DataDummy {
                 "The Flash",
                 "2014-10-07",
                 "After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only (meta-human) who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won't be long before the world learns what Barry Allen has become...The Flash.",
-                "77",
+                "7.7",
                 "https://image.tmdb.org/t/p/original/lJA2RCMfsWoskqlQhXPSLFQGXEJ.jpg"));
         show.add(new DataEntity("105971",
                 "The Bad Batch",
